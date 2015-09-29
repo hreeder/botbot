506f4fe9679626c9c7e09234c069a7046e44d808
Harry Reeder <harry@harryreeder.co.uk>
2015-09-29 15:25:30 +0100

    Add slack avatar support

--- hooks/slack.py
+++ hooks/slack.py
@@ -2,11 +2,15 @@ import json
 import re
 import requests
 
+from redis import StrictRedis
+
 
 def message_hook(bot, channel, sender, message):
     listen_on = bot.config['Slack']['listen'].split()
     if channel in listen_on and not sender.startswith("["):
-        print(channel, sender, message)
+        redis = StrictRedis.from_url(bot.config['System']['redis_url'])
+        redis_key = bot.config['System']['redis_prefix'] + "slack-avatar-" + sender
+
         endpoint = bot.config['Slack']['webhook']
         chanstr = channel.replace("#","")
         target_channel = bot.config['Slack'][chanstr+"_target"]
@@ -19,6 +23,9 @@ def message_hook(bot, channel, sender, message):
             'username': sender,
             'channel': target_channel
         }
+
+        if redis.exists(redis_key):
+            payload['icon_url'] = redis.get(redis_key)
 
         postit = requests.post(
             endpoint,
