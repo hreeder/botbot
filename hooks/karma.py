c98f0971e2a6b404abd618c5ef555f751dbf777b
Harry Reeder <harry@harryreeder.co.uk>
2016-01-21 10:48:54 +0000

    Punishment for adding karma to yourself

--- hooks/karma.py
+++ hooks/karma.py
@@ -1,29 +1,39 @@
 from redis import StrictRedis
 
+def increment(bot, term):
+    redis = StrictRedis.from_url(bot.config['System']['redis_url'])
+    if redis.hexists(bot.config['System']['redis_prefix'] + "karma", term):
+        oldvalue = int(redis.hget(bot.config['System']['redis_prefix'] + "karma", term))
+        redis.hset(bot.config['System']['redis_prefix'] + "karma", term, oldvalue + 1)
+    else:
+        redis.hset(bot.config['System']['redis_prefix'] + "karma", term, 1)
+
+def decremenet(bot, term):
+    redis = StrictRedis.from_url(bot.config['System']['redis_url'])
+    if redis.hexists(bot.config['System']['redis_prefix'] + "karma", term):
+        oldvalue = int(redis.hget(bot.config['System']['redis_prefix'] + "karma", term))
+        redis.hset(bot.config['System']['redis_prefix'] + "karma", term, oldvalue - 1)
+    else:
+        redis.hset(bot.config['System']['redis_prefix'] + "karma", term, -1)
+
 
 def message_hook(bot, channel, sender, message):
     redis = None
     term = None
 
     if message.endswith("++"):
-        redis = StrictRedis.from_url(bot.config['System']['redis_url'])
         term = message[:-2]
 
-        if redis.hexists(bot.config['System']['redis_prefix'] + "karma", term):
-            oldvalue = int(redis.hget(bot.config['System']['redis_prefix'] + "karma", term))
-            redis.hset(bot.config['System']['redis_prefix'] + "karma", term, oldvalue + 1)
+        if term == sender:
+            bot.message(channel, "Haha, nope!")
+            decremenet(bot, term)
         else:
-            redis.hset(bot.config['System']['redis_prefix'] + "karma", term, 1)
+            increment(bot, term)
     elif message.endswith("--"):
-        redis = StrictRedis.from_url(bot.config['System']['redis_url'])
         term = message[:-2]
+        decremenet(bot, term)
 
-        if redis.hexists(bot.config['System']['redis_prefix'] + "karma", term):
-            oldvalue = int(redis.hget(bot.config['System']['redis_prefix'] + "karma", term))
-            redis.hset(bot.config['System']['redis_prefix'] + "karma", term, oldvalue - 1)
-        else:
-            redis.hset(bot.config['System']['redis_prefix'] + "karma", term, -1)
-
-    if redis:
+    if term:
+        redis = StrictRedis.from_url(bot.config['System']['redis_url'])
         amount = int(redis.hget(bot.config['System']['redis_prefix'] + "karma", term))
         bot.message(channel, "%s now has %s karma" % (term, amount))
