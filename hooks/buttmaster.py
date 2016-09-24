5a663de20607dc0bad6a297502c78c9df5282112
Harry Reeder <harry@harryreeder.co.uk>
2016-09-24 01:43:05 +0100

    Use lowercase message

--- hooks/buttmaster.py
+++ hooks/buttmaster.py
@@ -2,7 +2,7 @@ from redis import StrictRedis
 
 
 def message_hook(bot, channel, sender, message):
-    if "butt" in message and sender not in ['buttbot']:
+    if "butt" in message.lower() and sender not in ['buttbot']:
         redis = StrictRedis.from_url(bot.config['System']['redis_url'])
 
         if redis.hexists(bot.config['System']['redis_prefix'] + "buttmaster", sender):
