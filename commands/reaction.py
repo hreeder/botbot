5d1dbf0eb5b341e55d839348e78bbee0b8af7552
Harry Reeder <harry@harryreeder.co.uk>
2017-01-09 11:13:10 +0000

    terms should be lowercase

--- commands/reaction.py
+++ commands/reaction.py
@@ -3,7 +3,7 @@ from redis import StrictRedis
 
 def react(bot, channel, sender, args):
     redis = StrictRedis.from_url(bot.config['System']['redis_url'])
-    term = "_".join(args)
+    term = "_".join(args).lower()
     try:
         url = redis.srandmember(bot.config['System']['redis_prefix'] + "reactions:" + term).decode('utf-8')
         bot.message(channel, url)
@@ -17,5 +17,5 @@ def add_reaction(bot, channel, sender, args):
     if len(args) == 1:
         bot.message(channel, "You did not specify a description")
     url = args[0]
-    term = "_".join(args[1:])
+    term = "_".join(args[1:]).lower()
     redis.sadd(bot.config['System']['redis_prefix'] + "reactions:" + term, url)
