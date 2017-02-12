22564c5c7e3cfe511fb9eaf7d7567a28a979db35
Harry Reeder <harry@harryreeder.co.uk>
2017-02-12 22:19:17 +0000

    Add bunny bombs

--- ircbot/plugins/bunny.py
+++ ircbot/plugins/bunny.py
@@ -14,3 +14,18 @@ def bunny_command(bot, channel, sender, args):
             pass
     data = requests.get(uri % part).json()
     bot.message(channel, "https://bunnies.io/#%s - %s" % (data['id'], data['media']['gif']))
+
+
+@bot.command('bunnybomb')
+def bunny_bomb(bot, channel, sender, args):
+    """ Usage: $bunnybomb [N] - Sends N bunnies to the current channel. N defaults to 5. Nmax = 10 """
+    n = 5
+    if args:
+        try:
+            n = int(args[0])
+        except ValueError:
+            pass
+    if n > 10:
+        n = 10
+    for _ in range(n):
+        bunny_command(bot, channel, sender, None)
