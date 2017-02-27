340889fcfa987e497e28616fbeed780e79585ae8
Harry Reeder <harry@harryreeder.co.uk>
2017-02-27 19:43:38 +0000

    Strip out newlines

--- ircbot/plugins/links.py
+++ ircbot/plugins/links.py
@@ -12,6 +12,8 @@ def link_title_parse_hook(bot, channel, sender, message):
             if rfc3987.match(word, rule='URI'):
                 r = requests.get(word)
                 soup = BeautifulSoup(r.text, 'html.parser')
-                bot.message(channel, " :: {}".format(soup.head.title.text))
+                title = soup.head.title.text.strip()
+                title = title.replace("\n", "")
+                bot.message(channel, " :: {}".format(title))
         except requests.exceptions.InvalidSchema:
             pass
