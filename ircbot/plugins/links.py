937acbc78ffb190b5291fcd78b37fb099172d3af
Harry Reeder <harry@harryreeder.co.uk>
2017-05-28 16:01:44 +0100

    Add whitelist/blacklist for channels on links

--- ircbot/plugins/links.py
+++ ircbot/plugins/links.py
@@ -4,9 +4,25 @@ import rfc3987
 from bs4 import BeautifulSoup
 from ircbot import bot
 
+def allowed_to_process(bot, channel):
+    mode = bot.config['Links']['mode']
+
+    if mode == "whitelist" and channel in bot.config['Links']['channels']:
+        return True
+    elif mode == "whitelist":
+        return False
+
+    if mode == "blacklist" and channel in bot.config['Links']['channels']:
+        return False
+
+    return True
+
 
 @bot.hook()
 def link_title_parse_hook(bot, channel, sender, message):
+    if not allowed_to_process(bot, channel):
+        return
+
     for word in message.split(" "):
         try:
             if rfc3987.match(word, rule='URI'):
