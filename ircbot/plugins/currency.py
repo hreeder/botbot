e83c5f393d186f5e345e64c777788f60c82f15b5
cazagen <sharpcameron@hotmail.co.uk>
2017-09-08 20:50:38 +0100

    It works

---
+++ ircbot/plugins/currency.py
@@ -0,0 +1,32 @@
+import json
+
+from urllib.request import urlopen
+
+from ircbot import bot
+
+
+def get_currency(currency_a, currency_b):
+    endpoint = "http://api.fixer.io/latest?base={}".format(str(currency_a).upper())
+
+    response =  urlopen(endpoint).read().decode("utf-8")
+    data = json.loads(response)
+
+    currency_ratio = data["rates"][str(currency_b).upper()]
+
+    return currency_ratio
+
+
+@bot.command('currency')
+def command(bot, channel, sender, args):
+    """Converts Currency. Usage: $currency 10usd gbp"""
+    currency_a = str(args[0])
+    amount_a = currency_a[:-3]
+    currency_a = currency_a[-3:]
+
+    currency_b = str(args[1])
+
+    currency_ratio = get_currency(currency_a, currency_b)
+
+    amount_b = float(amount_a) * currency_ratio
+
+    bot.message(channel, "{}{} is equal to {}{}".format(str(amount_a),currency_a.upper(),str(round(amount_b,2)),currency_b.upper()))
