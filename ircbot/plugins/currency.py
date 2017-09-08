01b8d3fadba9ed0a72b523840a4f2eeb8b1b6250
cazagen <sharpcameron@hotmail.co.uk>
2017-09-08 21:02:29 +0100

    Check number of arguments, and pep8

--- ircbot/plugins/currency.py
+++ ircbot/plugins/currency.py
@@ -8,7 +8,7 @@ from ircbot import bot
 def get_currency(currency_a, currency_b):
     endpoint = "http://api.fixer.io/latest?base={}".format(str(currency_a).upper())
 
-    response =  urlopen(endpoint).read().decode("utf-8")
+    response = urlopen(endpoint).read().decode("utf-8")
     data = json.loads(response)
 
     currency_ratio = data["rates"][str(currency_b).upper()]
@@ -19,6 +19,9 @@ def get_currency(currency_a, currency_b):
 @bot.command('currency')
 def command(bot, channel, sender, args):
     """Converts Currency. Usage: $currency 10 usd gbp"""
+    if len(args) < 3:
+        bot.message(channel, "This command requires 3 arguments.")
+        return
     amount_a = args[0]
     currency_a = str(args[1])
     currency_b = str(args[2])
@@ -27,4 +30,4 @@ def command(bot, channel, sender, args):
 
     amount_b = float(amount_a) * currency_ratio
 
-    bot.message(channel, "{}{} is equal to {}{}".format(str(amount_a),currency_a.upper(),str(round(amount_b,2)),currency_b.upper()))
+    bot.message(channel, "{}{} is equal to {}{}".format(str(amount_a), currency_a.upper(), str(round(amount_b, 2)), currency_b.upper()))
