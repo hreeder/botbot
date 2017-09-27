57a2790348e44932f123d9e81b52cd454f7fd8a7
William Mathewson <neanias@users.noreply.github.com>
2017-09-27 14:55:05 +0100

    Add more dogs

--- ircbot/plugins/dogs.py
+++ ircbot/plugins/dogs.py
@@ -3,8 +3,9 @@ import requests
 from ircbot import bot
 
 
-@bot.command('dog')
-def dog_command(bot, channel, sender, args):
+@bot.command(['dog', 'dogs'])
+def dog_command(bot, channel, sender, args=1):
+    """Get dog gifs! {bot.trigger}dog[s] [number of dog gifs]"""
     uri = "https://random.dog/woof.json"
-    data = requests.get(uri).json()
-    bot.message(channel, "{}".format(data['url']))
+    data = [requests.get(uri).json()['url'] for _ in range(args)]
+    bot.message(channel, " - ".join(data))
