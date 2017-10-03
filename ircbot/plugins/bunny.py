96362e9a588c0a6dc9aef0e9274a190d2ed0960c
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-10-03 19:10:41 +0100

    I can't Git properly

--- ircbot/plugins/bunny.py
+++ ircbot/plugins/bunny.py
@@ -5,12 +5,8 @@ from ircbot import bot
 
 @bot.command('bunny')
 def bunny_command(bot, channel, sender, args):
-<<<<<<< HEAD
-    uri = "https://api.bunnies.io/v2/loop/{}/?media=gif"
-=======
     """Usage: {bot.trigger}bunny [bunny id] - Grabs a bunny gif, if an id isn't specifed one will be chosen"""
-    uri = "https://api.bunnies.io/v2/loop/%s/?media=gif"
->>>>>>> bb1bbd37bb51e6f76bc3405e5b31138b887faf0f
+    uri = "https://api.bunnies.io/v2/loop/{}/?media=gif"
     part = "random"
     if args:
         try:
