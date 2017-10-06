5465735d3ca578a79a4d0f8a1c15d49c30865f01
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-10-06 13:42:08 +0100

    You can now request a random entry from urban dictionary

--- ircbot/plugins/urban.py
+++ ircbot/plugins/urban.py
@@ -11,3 +11,12 @@ def ud(bot, channel, sender, args):
         definition.word,
         definition.definition
     ))
+
+@bot.command('udrandom')
+def udrandom(bot, channel, sender, args):
+    """ Defines you a random word from urban dictonary"""
+    definition = urbandictionary.random()
+    bot.message(channel, "{}: {}".format(
+        definition[0].word,
+        definition[0].definition
+    ))
