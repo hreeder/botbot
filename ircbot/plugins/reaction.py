9605e90b0b6dc3fffd25e086b8cf92cff50a7ec6
Harry Reeder <harry@harryreeder.co.uk>
2017-10-17 18:55:35 +0100

    eyebrows

--- ircbot/plugins/reaction.py
+++ ircbot/plugins/reaction.py
@@ -15,10 +15,16 @@ def react(bot, channel, sender, args):
 
 @bot.command('addreaction')
 def add_reaction(bot, channel, sender, args):
-    """ Adds a reaction for a term - usage: $addreaction http://your.url.here your description here """
+    """ Adds a reaction for a term - usage: {bot.trigger}addreaction http://your.url.here your description here """
     redis = StrictRedis.from_url(bot.config['System']['redis_url'])
     if len(args) == 1:
         bot.message(channel, "You did not specify a description")
     url = args[0]
     term = "_".join(args[1:]).lower()
     redis.sadd(bot.config['System']['redis_prefix'] + "reactions:" + term, url)
+
+
+@bot.command(';)')
+def react_shortcut_eyebrows(bot, channel, sender, args):
+    """ Shortcut for {bot.trigger}react eyebrows """
+    react(bot, channel, sender, ["eyebrows"])
