cf89f66f76b2c2ea930b9b689a96cc816fb4db80
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-11-27 17:14:33 +0000

    Feature: Dad jokes

---
+++ ircbot/plugins/dadjoke.py
@@ -0,0 +1,13 @@
+import requests
+
+from ircbot import bot
+
+@bot.command('dadjoke')
+def dad_joke(bot, channel, sender, args):
+    """Return a random dad - Usage: {bot.trigger}dadjoke"""
+    endpoint = "https://icanhazdadjoke.com/"
+    data = requests.get(endpoint, headers={"Accept": "application/json"}).json()
+
+    response = data['joke']
+
+    bot.message(channel, "{}: {}".format(sender, response))
