8ee2bbde0728095d3efffe1fc1106a5932b95065
Findlay Smith <findlayksmith@gmail.com>
2018-03-17 18:18:53 +0100

    Update karma.py
    actually might pass this time

--- ircbot/plugins/karma.py
+++ ircbot/plugins/karma.py
@@ -72,7 +72,7 @@ def message_hook(bot, channel, sender, message):
         term = m.group('term')
         reason = m.group('reason')
         term = term.strip().lower()
-        if term == sender.lower():
+        if term == sender.lower() or term + "~d" == sender.lower():
             bot.message(channel, "Haha, nope!")
             decrement(bot, term)
         else:
