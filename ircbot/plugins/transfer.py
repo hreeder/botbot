72225d8ec17dbb780016fcb7b74d7e3e22988877
findlaysmith <ferelach45@gmail.com>
2018-04-10 13:19:16 +0100

    minor changes requested by tbrb

--- ircbot/plugins/transfer.py
+++ ircbot/plugins/transfer.py
@@ -6,24 +6,23 @@ from redis import StrictRedis
 
 @bot.command('transfer')
 def transfer(bot, channel, sender, args):
-    """"command to transfer karma & butts from one term to another"""
-
-    if len(args) == 2:
-            first_name = args[0].lower()
-            second_name = args[1].lower()
-    else:
-        string = args.join(" ")
-        strings = []
-        for term in re.findall("(?![\'\"])[a-z,\s]+(?=[\"\'])", string):
-            strings.append(term.strip())
-        try:
-            first_name = strings[0].lower()
-            second_name = strings[1].lower()
-        except IndexError:
-            bot.message(channel, "Oops! Looks like you didn't give enough arguments!")
-            return
-
+    """"command to transfer karma & butts from one term to another, usage: $transfer name1 name2"""
     if sender.lower() == bot.config['System']['owner']:
+        if len(args) == 2:
+                first_name = args[0].lower()
+                second_name = args[1].lower()
+        else:
+            string = " ".join(args)
+            strings = []
+            for term in re.findall("(?![\'\"])[a-z,\s]+(?=[\"\'])", string):
+                strings.append(term.strip())
+            try:
+                first_name = strings[0].lower()
+                second_name = strings[1].lower()
+            except IndexError:
+                bot.message(channel, "Oops! Looks like you didn't give enough arguments!, please use the following syntax: `$transfer name1 name2` ")
+                return
+
         redis = StrictRedis.from_url(bot.config['System']['redis_url'])
         # transfer karma
         try:
