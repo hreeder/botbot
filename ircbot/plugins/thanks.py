3283187e8200479e52706f3e127d370e62aa2227
William Mathewson <wncmathewson@me.com>
2018-05-23 23:42:12 +0100

    Change to match directly on regex objects

--- ircbot/plugins/thanks.py
+++ ircbot/plugins/thanks.py
@@ -3,8 +3,8 @@ import re
 
 from ircbot import bot
 
-THANKS_REGEX = re.compile(r"thank(s| you),? {}".format(bot.config['IRC']['nick']), re.IGNORECASE)
-FUCK_REGEX = re.compile(r"fuck you,? {}".format(bot.config['IRC']['nick']), re.IGNORECASE)
+THANKS_REGEX = re.compile(r"^thank(s| you),? {}$".format(bot.config['IRC']['nick']), re.IGNORECASE)
+FUCK_REGEX = re.compile(r"^fuck you,? {}$".format(bot.config['IRC']['nick']), re.IGNORECASE)
 
 THANKS_REPLIES = [
     'No worries, {}!',
@@ -25,7 +25,7 @@ FUCK_REPLIES = [
 @bot.hook()
 def message_hook(bbot, channel, sender, message):
     """For when you want to say something to botbot!"""
-    if re.match(THANKS_REGEX, message):
+    if THANKS_REGEX.match(message.strip()):
         bbot.message(channel, random.choice(THANKS_REPLIES).format(sender))
-    elif re.match(FUCK_REGEX, message):
+    elif FUCK_REGEX.match(message.strip()):
         bbot.message(channel, random.choice(FUCK_REPLIES).format(sender))
